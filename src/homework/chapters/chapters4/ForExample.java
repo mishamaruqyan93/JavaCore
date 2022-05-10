f805669c925446af8290602e00a2520623bc1c04
SmartS <marukyan93@inbox.ru>
2022-05-10 22:55:38 +0300

    Added ForExample class.

---
+++ src/homework/chapters/chapters4/ForExample.java
@@ -0,0 +1,12 @@
+package homework.chapters.chapters4;
+
+public class ForExample {
+    public static void main(String[] args) {
+        for (int i = 0; i < 5; i++) {
+            for (int j = 5; j > i; j--) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+}
